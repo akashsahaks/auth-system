17362db58771b38092265aa0c6b6d874151c98de
akash-sah-aks <akashsah.aks@gmail.com>
2023-11-28 14:18:23 +0530

    enable cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRouter = require("../router/authRoute");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRouter);
 
